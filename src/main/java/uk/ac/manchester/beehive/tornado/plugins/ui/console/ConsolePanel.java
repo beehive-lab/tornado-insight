14a6b748fe3658654fc79895ac86f8a9927e99be
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2025-04-11 16:02:34 +0300

    Marked deprecated methods and reformated classes

--- src/main/java/uk/ac/manchester/beehive/tornado/plugins/ui/console/ConsolePanel.java
+++ src/main/java/uk/ac/manchester/beehive/tornado/plugins/ui/console/ConsolePanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, APT Group, Department of Computer Science,
+ * Copyright (c) 2023, 2025, APT Group, Department of Computer Science,
  *  The University of Manchester.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,16 +21,17 @@ import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.DataKey;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindow;
-import uk.ac.manchester.beehive.tornado.plugins.util.DataKeys;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import uk.ac.manchester.beehive.tornado.plugins.util.DataKeys;
 
 public class ConsolePanel extends SimpleToolWindowPanel implements DataProvider {
     private final ConsoleView consoleView;
@@ -53,9 +54,13 @@ public class ConsolePanel extends SimpleToolWindowPanel implements DataProvider 
         }
     }
 
+    /**
+     * @deprecated Use {@link #getData(DataKey)} instead.
+     */
+    @Deprecated
     @Override
     public @Nullable Object getData(@NotNull @NonNls String dataId) {
-        if (DataKeys.TORNADO_CONSOLE_VIEW.is(dataId)){
+        if (DataKeys.TORNADO_CONSOLE_VIEW.is(dataId)) {
             return consoleView;
         }
         return super.getData(dataId);
